f5de1e6c9f966ec2d2b5e70c9da30eb8acc94b82
Katrejs <167353445+Katrejs@users.noreply.github.com>
2025-04-28 05:11:04 +0300

    Update Invite.js

--- database/Invite.js
+++ database/Invite.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const Invite = mongoose.Schema({
+  _id: String,
+  inviterid: String,
+  tarih: String
+});
+
+module.exports = mongoose.model("Invite", Invite);
